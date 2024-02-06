5ea4dbb06700726051999528bae217a0065a1ee9
JormaByte <joel.oksanen1223@gmail.com>
2024-02-06 13:09:58 +0200

    stopping the interval with a button

--- week6/use_effect_and_timers/App.js
+++ week6/use_effect_and_timers/App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator, Button } from 'react-native';
 
 export default function App() {
 
-  const [isPending, setPending] = useState(true)
+/*  
+EXAMPLE 1: LOG & ACTIVITY
+
+const [isPending, setPending] = useState(true)
 
   useEffect(() => {
     setTimeout(() => {
@@ -22,7 +25,24 @@ export default function App() {
       {!isPending && <Text>Time is up!</Text>}
       <StatusBar style="auto" />
     </View>
-  );
+  ); */
+
+  const [count, setCount] = useState(0)
+  const [timerId, setTimerId] = useState(0)
+
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      setCount( prev => prev + 1)
+    }, 1000)
+    setTimerId(timerId)
+  }, [])
+
+  return(
+    <View>
+      <Text style={{fontSize: 40}}>{count}</Text>
+      <Button title='STOP' onPress={() => clearInterval(timerId)} />
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
