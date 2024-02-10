70d522c1d77fdc40651f98362a5f66eed31dfaae
JormaByte <joel.oksanen1223@gmail.com>
2024-02-10 13:37:39 +0200

    isValidEndDay function

--- booking/App.js
+++ booking/App.js
@@ -1,14 +1,73 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { Container, Row, Col } from 'react-native-flex-grid';
+import DateTimePicker from '@react-native-community/datetimepicker'
+
+
+const [date, setDate] = useState(new Date())
+const [startDate, setStartDate] = useState('')
+const [endDate, setEndDate] = useState('')
+const [startTime, setStartTime] = useState('')
+const [endTime, setEndTime] = useState('')
+const [status, setStatus] = useState('Pick start date & time')
+const [mode, setMode] = useState('date')
+const [show, setShow] = useState(false)
 
 export default function App() {
+
+  const showMode = (currentMode) => {
+    setShow(true)
+    setMode(currentMode)
+  }
+  
+  const showDatePicker = () => {
+    showMode('date')
+  }
+
+  const showTimePicker = () => {
+    showMode('time')
+  }
+
+/*
+isValidEndDay() takes one argument that is the selected date of the
+end of the booking. Dates are saved to correspondent state variables as strings
+using format dd.MM.yyyy. JavaScript function split() is used to split date
+strings to three parts (day, month, year) according to separator. Then year,
+month and day will be compared in order to check that end date is not before
+the start date
+*/
+
+const isValidEndDay = (newDate) => {
+  let start = startDate.split('.')
+  let end = endDate.split('.')
+  if (parseInt(end[2]) < parseInt(start[1])) {
+    return false
+  }
+  else if (parseInt(end[2]) === parseInt(start[2]) &&
+  parseInt(end[1]) < parseInt(start[1])) {
+    return false
+  }
+  else if (parseInt(end[2]) === parseInt(start[2]) &&
+  parseInt(end[1]) === parseInt(start[1]) &&
+  parseInt(end[0]) < parseInt(start[0])) {
+    return false
+  }
+  else {
+    return true
+  }
+}
+
+
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
