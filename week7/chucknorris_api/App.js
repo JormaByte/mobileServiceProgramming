14d5903b2bdd6a6e56131127c18137a1b297692a
JormaByte <joel.oksanen1223@gmail.com>
2024-02-12 19:14:34 +0200

    first release

--- week7/chucknorris_api/App.js
+++ week7/chucknorris_api/App.js
@@ -1,15 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Button, ActivityIndicator, Image } from 'react-native';
 
 const URL = 'https://api.chucknorris.io/jokes/random'
+const imgURL = 'https://api.chucknorris.io/img/chucknorris_logo_coloured_small@2x.png'
+
+
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+  const [joke, setJoke] = useState('')
+  const [isLoading, setIsLoading] = useState(true)
+  const [error, setError] = useState(null)
+  const [refresh, setRefresh] = useState()
+
+
+  const getNewJoke = () => {
+    setRefresh({})
+  }
+
+  useEffect(() => {
+    fetch(URL)
+    .then(response => response.json())
+    .then ((json) => {
+      console.log(json);
+      setJoke(firstLetterToLowerCase(json.value))
+      setError(null)
+      setIsLoading(false)
+    }, (error) => {
+      setError('Error retrieving joke!')
+      setIsLoading(false)
+      console.log(error);
+    })
+  }, [refresh])
+
+  const firstLetterToLowerCase = (str) => {
+    return str.charAt(0).toLowerCase() + str.slice(1)
+  }
+
+  if (isLoading) {
+    return <View style={styles.container}>
+      <ActivityIndicator size='large' />
     </View>
-  );
+  } else if (error) {
+    return <View style={styles.container}>
+      <Text>{error}</Text>
+    </View>
+  } else {
+    return (
+      <View style={styles.container}>
+        <Image source={{uri: imgURL}}/>
+        <Text style={styles.activity}>
+          {joke}
+        </Text>
+        <Button title='Refresh' onPress={() => getNewJoke()} />
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
