c27865906d31b15829bc17836fec352b2e62fc61
JormaByte <joel.oksanen1223@gmail.com>
2024-02-12 14:19:13 +0200

    added state variables

--- week7/bored_api/App.js
+++ week7/bored_api/App.js
@@ -1,5 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+const URL = 'https://www.boredapi.com/api/activity'
+const [activity, setActivity] = useState('')
+const [type, setType] = useState('')
+const [participants, setParticipants] = useState(0)
+const [price, setPrice] = useState(0)
+const [isLoading, setIsLoading] = useState(true)
+const [error, setError] = useState(null)
+const [refresh, setRefresh] = useState()
 
 export default function App() {
   return (
