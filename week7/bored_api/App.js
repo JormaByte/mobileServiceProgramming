ee7136c9efc3d671a7b8f77cfb1e99b153b95591
JormaByte <joel.oksanen1223@gmail.com>
2024-02-12 14:26:13 +0200

    useEffect

--- week7/bored_api/App.js
+++ week7/bored_api/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
 const URL = 'https://www.boredapi.com/api/activity'
@@ -12,6 +12,31 @@ const [error, setError] = useState(null)
 const [refresh, setRefresh] = useState()
 
 export default function App() {
+
+/*
+Javascript’s fetch-then-then structure is used to call API through HTTPS. Fetch
+executes the call and after that result is converted into JSON (first then) and
+processed (second then). API call is executed asynchronously i.e. on the
+background. JSON path is used to read the activity
+*/
+useEffect(() => {
+  fetch(URL)
+  .then(response => response.json())
+  .then ((json) => {
+    console.log(json);
+    setActivity(firstLetterToLowerCase(json.activity))
+    setType(json.type)
+    setParticipants(json.participants)
+    setPrice(json.price)
+    setError(null)
+    setIsLoading(false)
+  }, (error) => {
+    setError('Error retrieving activity!')
+    setIsLoading(false)
+    console.log(error);
+  })
+}, [refresh])
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
