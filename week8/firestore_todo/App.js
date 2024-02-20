104bd11391b3a16b32d6ffb6647cf08a25e741dc
JormaByte <joel.oksanen1223@gmail.com>
2024-02-20 13:14:59 +0200

    imports

--- week8/firestore_todo/App.js
+++ week8/firestore_todo/App.js
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { Text, View } from 'react-native';
+import styles from './styles/style'
+import { addDoc, collection, deleteDoc,
+doc, getDocs, onSnapshot, orderBy, query } from 'firebase/firestore';
+import { db, TODOS_REF } from './firebase/Config'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
